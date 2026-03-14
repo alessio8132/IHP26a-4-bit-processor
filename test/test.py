cbf56738ac8048ee7a0acd4b76d0be49ee0ff2b7
Alessio <alessio.widmer@gmail.com>
2026-03-14 13:37:49 +0100

    Testing should work now

--- test/test.py
+++ test/test.py
@@ -26,15 +26,17 @@ async def test_project(dut):
     dut._log.info("Test project behavior")
 
     # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
+    dut.ui_in.value = 0b00010011 # Load value 3 into the accumulator
+    await ClockCycles(dut.clk, 1)
+    dut.ui_in.value = 0b00100001 # Add value 1 to the accumulator
+    await ClockCycles(dut.clk, 1)
+    dut.ui_in.value = 0b10000000 # Output the value of the accumulator to uo_out
     # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    assert dut.uo_out.value == 0b00100100 # Expected output: PC = 2, output_register = 4
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
